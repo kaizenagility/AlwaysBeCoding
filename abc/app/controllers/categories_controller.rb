7092efb7ef7ba57cb7f286d7c7a891e5456b9374
Kai Zhang <kaizen.agility@gmail.com>
2017-12-17 00:43:40 -0500

    Generated Categories model, controller, routes

---
+++ abc/app/controllers/categories_controller.rb
@@ -0,0 +1,28 @@
+class CategoriesController < ApplicationController
+  def index
+    render json: Category.all
+  end
+
+  def show
+    render json: Category.find(params[:id])
+  end
+
+  def create
+    category = Category.new(name: params[:name],
+                  user_id: params[:user_id])
+    category.save
+    render json: {status: "new category successful", category: category}
+  end
+
+  def destroy
+    Category.destroy(params[:id])
+    render json: {status: "delete successful"}
+  end
+
+  def update
+    log = Cateogry.update(params[:id],
+                    name: params[:name],
+                    user_id: params[:user_id])
+    render json: {status: "category update successful", category: category}
+  end
+end
